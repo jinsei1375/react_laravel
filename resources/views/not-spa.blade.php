5e07aedda717d5bd9c60352617252a2eb58ce17d
Shinsei <murakami.shinsei.zn@gmail.com>
2024-08-10 17:02:26 +0900

    reactではjsxのコンパイルが必要なのでbabelを使用

--- resources/views/not-spa.blade.php
+++ resources/views/not-spa.blade.php
@@ -6,6 +6,7 @@
     </div>
     <script src="https://unpkg.com/react@17/umd/react.production.min.js"></script>
     <script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>
+    {{-- reactの場合はコンパイルが必要なので、babelを使ってコンパイルする --}}
     <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
     <script type="text/babel">
         const { useState } = React;
