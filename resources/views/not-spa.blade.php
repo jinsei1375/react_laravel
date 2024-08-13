9dd3d2a65d1b8fe69472d4c9a3f0d903dd5f0197
murakami <murakami@kokochie.jp>
2024-08-13 10:33:48 +0900

    reactに組み込み

--- resources/views/not-spa.blade.php
+++ resources/views/not-spa.blade.php
@@ -4,25 +4,12 @@
         <p>bladeエリア</p>
         <a href="{{route('posts.index')}}">投稿一覧</a>
     </div>
-    <script src="https://unpkg.com/react@17/umd/react.production.min.js"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    {{-- <script type="module" src="{{ mix('js/app.js') }}"></script> --}}
+    {{-- <script src="https://unpkg.com/react@17/umd/react.production.min.js"></script>
     <script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>
     {{-- reactの場合はコンパイルが必要なので、babelを使ってコンパイルする --}}
-    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+    {{-- <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script> 
     <script type="text/babel">
-        const { useState } = React;
-
-        const ExampleComponent = () => {
-            const [flg, setFlg] = useState(true);
-
-            return (
-                <div>
-                    <p>Reactエリア</p>
-                    <h1>{flg ? 'ture' : 'flse'}</h1>
-                    <button onClick={() => setFlg(!flg)}>Click</button>
-                </div>
-            );
-        };
-
-        ReactDOM.render(<ExampleComponent />, document.getElementById('example'));
-    </script>
+    </script> --}}
 </x-app-layout>
